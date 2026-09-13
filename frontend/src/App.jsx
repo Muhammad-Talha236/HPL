027f41afa0a2cf3d76978ea9c335e6f104328627
Muhammad-Talha236 <mtalha.mt236@gmail.com>
2026-09-13 16:45:30 +0500

    Add initial setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,17 @@
+
+
+function App() {
+ 
+
+  return (
+   <>
+       <div className="App bg-amber-50">
+           <h1> Hunza Premier League
+           </h1>
+           <h2>Welcome to the Hunza Premier League</h2>
+      </div>
+   </>
+  )
+}
+
+export default App
